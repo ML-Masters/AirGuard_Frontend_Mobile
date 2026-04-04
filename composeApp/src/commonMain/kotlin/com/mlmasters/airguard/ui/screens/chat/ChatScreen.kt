1336b5bdccd3a2d15d615ac4148401ff520fcfe8
Scorpion_3.0 <tiomelajorel@gmail.com>
2026-04-04 01:05:08 +0100

    Fix: chat input padding - move imePadding to input surface only

--- composeApp/src/commonMain/kotlin/com/mlmasters/airguard/ui/screens/chat/ChatScreen.kt
+++ composeApp/src/commonMain/kotlin/com/mlmasters/airguard/ui/screens/chat/ChatScreen.kt
@@ -36,8 +36,7 @@ fun ChatScreen(viewModel: ChatViewModel = koinViewModel()) {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background)
-            .imePadding(),
+            .background(MaterialTheme.colorScheme.background),
     ) {
         // Header
         Surface(
@@ -112,6 +111,7 @@ fun ChatScreen(viewModel: ChatViewModel = koinViewModel()) {
         Surface(
             color = MaterialTheme.colorScheme.surface,
             shadowElevation = 8.dp,
+            modifier = Modifier.imePadding(),
         ) {
             Row(
                 modifier = Modifier
